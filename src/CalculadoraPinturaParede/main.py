2e52ff7d631fe8aa22040bfc9e760ba49fa8cf35
Jak3751 <jak3751@gmail.com>
2023-12-08 15:17:28 -0400

    go

--- src/CalculadoraPinturaParede/main.py
+++ src/CalculadoraPinturaParede/main.py
@@ -1,8 +1,13 @@
-largura: float = input("Informe largura :")
-altura: float = input("Informe altura :")
-profundidade: float = input("Informe profundidade :")
+largura: float = float(input("Informe largura do cômodo em metros: "))
+altura: float = float(input("Informe altura do cômodo em metros: "))
+profundidade: float = float(input("Informe profundidade do cômodo em metros: "))
 
-print(float(2 * (largura + profundidade) * altura))
+area_paredes: float = 2 * (largura + profundidade) * altura
 
-print(float(largura * profundidade))
+print("A área das paredes é: ", area_paredes , "metros")
 
+area_teto: float = largura * profundidade
+
+print("A área do teto é: ", area_teto , "metros")
+
+print("Sabendo que a cada 1 litro de tinta rende 10 metros de pintura, a litragem de tinta necessária é: ", (area_paredes + area_teto) / 10 , "litros")
