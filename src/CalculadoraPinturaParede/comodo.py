5119b0b92610b13a9592707abf88094edd7d5f37
Jak3751 <jak3751@gmail.com>
2023-12-12 16:18:00 -0400

    go

--- src/CalculadoraPinturaParede/comodo.py
+++ src/CalculadoraPinturaParede/comodo.py
@@ -1,9 +1,48 @@
 class Comodo:
-    altura: float
-    largura: float
-    profundidade: float
+    __altura: float
+    __largura: float
+    __profundidade: float
 
     def __init__(self, altura, largura, profundidade):
-        self.altura = float(altura)
-        self.largura = float(largura)
-        self.profundidade = float(profundidade)
+        self.altura = altura
+        self.largura = largura
+        self.profundidade = profundidade
+
+    @property
+    def altura(self):
+        return self.__altura
+    
+    @property
+    def largura(self):
+        return self.__largura
+    
+    @property
+    def profundidade(self):
+        return self.__profundidade
+    
+    @altura.setter
+    def altura(self, altura):
+        try:
+            self.__altura = float(altura)
+        except Exception:
+            print('O valor informado da altura é inválido!')
+            exit()
+
+    @largura.setter
+    def largura(self, largura):
+        try:
+            self.__largura = float(largura)
+        except Exception:
+            print('O valor informado da largura é inválido!')
+            exit()
+
+    @profundidade.setter
+    def profundidade(self, profundidade):
+        try:
+            self.__profundidade = float(profundidade)
+        except Exception:
+            print('O valor informado da profundidade é inválido')
+            exit()
+
+
+
