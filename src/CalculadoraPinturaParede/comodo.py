2e3e55d4109121a01b7184792921419b81fbfcd0
jak3751 <jak3751@gmail.com>
2023-12-12 06:43:47 -0300

    go

---
+++ src/CalculadoraPinturaParede/comodo.py
@@ -0,0 +1,9 @@
+class Comodo:
+    altura: float
+    largura: float
+    profundidade: float
+
+    def __init__(self, altura, largura, profundidade):
+        self.altura = float(altura)
+        self.largura = float(largura)
+        self.profundidade = float(profundidade)
---
+++ src/CalculadoraPinturaParede/main.py
@@ -0,0 +1,18 @@
+from calculadora import Calculadora
+from comodo import Comodo
+
+calc = Calculadora()
+
+comodo = Comodo(
+    input('Qual altura do cômodo, em metros? '),
+    input('Qual largura do cômodo, em metros? '),
+    input('Qual profundidade do cômodo, em metros ? ')
+)
+print("A área das paredes é: ",
+    calc.calcular_area_paredes(comodo), "metros")
+
+print("A área do teto é: ",
+    calc.calcular_area_teto(comodo), "metros")
+
+print("Sabendo que a cada 1 litro de tinta rende 10 metros de pintura, a litragem de tinta necessária é: ", 
+    calc.calcular_litragem_necessaria(), "litros")
