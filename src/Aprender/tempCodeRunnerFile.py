23fc41184c329a5ab74115132b76878911e0086f
Jak3751 <jak3751@gmail.com>
2023-12-16 07:32:29 -0400

    go

--- src/Aprender/tempCodeRunnerFile.py
+++ src/Aprender/tempCodeRunnerFile.py
@@ -1,8 +1,19 @@
-import turtle as t
-from random import random
+import re
 
-for i in range(100):
-    steps = int(random() * 100)
-    angle = int(random() * 360)
-    t.right(angle)
-    t.fd(steps)
+def extrair_numero_sete_digitos(texto):
+    padrao = r'\b\d{7}\b'
+    correspondencias = re.findall(padrao, texto)
+    
+    if correspondencias:
+        return correspondencias[0]
+    else:
+        return None
+
+# Exemplo de uso
+texto_exemplo = "Este é um exemplo com o número 1234567 e outros números."
+numero_sete_digitos = extrair_numero_sete_digitos(texto_exemplo)
+
+if numero_sete_digitos:
+    print(f"Número de 7 dígitos encontrado: {numero_sete_digitos}")
+else:
+    print("Nenhum número de 7 dígitos encontrado.")
