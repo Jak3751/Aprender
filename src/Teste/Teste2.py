cddbc8078e96e2dee4923ad9b2f0597a6de56477
jak3751 <jak3751@gmail.com>
2023-12-18 23:21:54 -0300

    go

---
+++ src/Teste/Teste2.py
@@ -0,0 +1,53 @@
+#Faça um algoritmo que solicite o nome do usuário e depois escreva o nome da pessoa no console.
+#nome = input("Informe seu nome :")
+#print(nome)
+
+
+#Faça um algoritmo que pergunte ao usuário quantos anos ele tem, depois disso, escreva True no console, caso ele já tenha alcançado a maioridade (18 anos), caso contrário escreva False.
+#idade = int(input("Quantos anos você tem ? "))
+#maioridade = idade >= 18
+#print(maioridade)
+
+
+#Faça um algoritmo que solicite um número ao usuário, depois disso, escreva True no console, caso o número tenha dois dígitos (Esteja entre 10 e 99), caso contrário escreva False.
+#numero = int(input("Informe um numero :"))
+#dois_digitos = numero >= 10 and numero <=99
+#print(dois_digitos)
+
+
+#Faça um algoritmo que solicite 3 notas para o usuário, calcule a média e indique se o aluno foi aprovado ou reprovado (nota precisar ser maior ou igual à sete para o aluno ser aprovado).
+#nota1 = float(input("Informe a primeira nota: "))
+#nota2 = float(input("informe a segunda nota: "))
+#nota3 = float(input("informe a terceira nota: "))
+
+#media = (nota1 + nota2 + nota3) / 3
+
+#if media >= 7:
+#    print("Aprovado")
+#else:
+#    print("Reprovado")
+
+
+#Faça um algoritmo que solicite o ano que o usuário nasceu, depois disso, faça o programa descrever se o usuário fará ou já fez 18 anos neste ano.
+
+
+
+#Faça um programa que solicite ao usuário sua idade, depois disso, exiba a classificação etária de acordo com as faixas de valores:
+
+#Faça um programa que solicite ao usuário 2 valores, utilize uma condição ternária para escrever qual o maior valor: o primeiro ou o segundo (caso os valores sejam iguais, considere o segundo).
+
+#Faça um programa que solicite o nome do usuário e depois disso faça uma saudação no formato: "Olá {nome digitado pelo usuário}".
+
+#Faça um programa que solicite uma mensagem qualquer para o usuário e exiba esta mensagem com todas as letras em maiúsculo.
+
+#Faça um programa que solicite a idade do usuário, verifique se o texto informado só contém números. Caso contenha somente números exiba a mensagem: "Você tem {idade digitada} anos.", caso contrário exiba a mensagem: "Você digitou uma idade inválida".
+
+#Faça um programa que solicite o nome completo do usuário e exiba somente o seu segundo nome/primeiro sobrenome.
+
+#Faça um programa que inicialize uma lista com o nome das pessoas da sua família.
+
+#Faça um programa que inicialize uma lista vazia e solicite ao usuário 3 nomes de cidades, um por vez, cada vez que o usuário digitar um nome, o programa deve incluir este nome na lista de cidades.
+
+#Faça um programa que inicialize uma lista com vários números diferentes, depois disso, solicite ao usuário um número, verifique se o número está ou não na lista e exiba uma mensagem notificando o usuário do resultado.
+
+#Faça um programa que inicialize uma lista vazia e a preencha com 5 nomes diferentes digitados pelo usuário, depois disso solicite um número de 0 até 4 e remova o elemento desta posição.
