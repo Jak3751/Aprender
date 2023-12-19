bb90bc1465567b68da9696aef6715040de9c15da
jak3751 <jak3751@gmail.com>
2023-12-18 21:28:07 -0300

    go

--- src/Teste/Teste.py
+++ src/Teste/Teste.py
@@ -10,7 +10,7 @@ else:
 #Faça um programa que receba duas notas digitadas pelo usuário. Se a nota for maior ou igual a seis, escreva aprovado, senão escreva reprovado.
     
 nota1 = float(input('Informe a primeira nota :'))
-nota2 = float(input('Informe a segunda nota: '))
+nota2 = float(input('Informe a segunda nota :'))
 
 media = (nota1 + nota2) / 2
 
@@ -18,8 +18,7 @@ media = (nota1 + nota2) / 2
 if media >= 6:
     print('Aprovado!')
 else:
-    print('reprovado')
-    
+    print('Reprovado!')
         
 #Escreva um programa que ordene uma lista numérica com três elementos.  
 
