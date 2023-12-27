3873619ebac38424168363214de4fe363be5244c
Jak3751 <jak3751@gmail.com>
2023-12-27 07:44:57 -0400

    go

--- src/Controle_Financeiro/lancamentos.py
+++ src/Controle_Financeiro/lancamentos.py
@@ -1,20 +1,20 @@
 class ControleDespesas:
     def __init__(self):
-        self.despesas = {}
+        self.__despesas = {}
 
     def adicionar_despesa(self, categoria, valor):
-        if categoria in self.despesas:
-            self.despesas[categoria].append(valor)
+        if categoria in self.__despesas:
+            self.__despesas[categoria].append(valor)
         else:
-            self.despesas[categoria] = [valor]
+            self.__despesas[categoria] = [valor]
         print("Despesa adicionada com sucesso.")
 
     def visualizar_despesas(self):
-        for categoria, valores in self.despesas.items():
+        for categoria, valores in self.__despesas.items():
             total_categoria = sum(valores)
             print(f"{categoria}: {valores} (Total: {total_categoria})")
 
     def calcular_total_despesas(self):
-        total = sum([sum(valores) for valores in self.despesas.values()])
+        total = sum([sum(valores) for valores in self.__despesas.values()])
         print(f"Total de despesas: {total}")
 
