2937bb16009200ffd276839a93574274cc8ed6d7
Jak3751 <jak3751@gmail.com>
2024-01-05 13:48:39 -0400

    go

--- src/Controle_Financeiro/main.py
+++ src/Controle_Financeiro/main.py
@@ -2,10 +2,11 @@ from funcoes import conectar_banco, lancar_despesa, excluir_despesa, consultar_d
 
 def menu():
     print("==== Sistema de Controle de Despesas ====")
-    print("1. Lançar Despesa")
-    print("2. Excluir Despesa")
-    print("3. Consultar Despesas")
-    print("4. Sair")
+    print("1. Cadastrar tipo Despesa")
+    print("2. Lançar Despesa")
+    print("3. Excluir Despesa")
+    print("4. Consultar Despesas")
+    print("5. Sair")
 
 def main():
     conexao = conectar_banco()
@@ -15,14 +16,14 @@ def main():
             menu()
             opcao = input("Escolha uma opção: ")
 
-            if opcao == "1":
+            if opcao == "2":
                 lancar_despesa(conexao)
-            elif opcao == "2":
+            elif opcao == "3":
                 id_despesa = int(input("Digite o ID da despesa a ser excluída: "))
                 excluir_despesa(conexao, id_despesa)
-            elif opcao == "3":
-                consultar_despesas(conexao)
             elif opcao == "4":
+                consultar_despesas(conexao)
+            elif opcao == "5":
                 print("Saindo do sistema.")
                 break
             else:
